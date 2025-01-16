3f2fd54224860fc5a8777e64665487e21c313c6f
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2025-01-16 17:13:45 -0300

    feat: Adicionar ToastContainer ao layout e remover do App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './theme/global.css'
 
 import { RouterProvider } from 'react-router-dom'
-import { ToastContainer } from 'react-toastify'
 
 import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './router'
@@ -9,7 +8,6 @@ import { router } from './router'
 function App() {
   return (
     <ThemeProvider storageKey="rocketshoes-theme" defaultTheme="dark">
-      <ToastContainer />
       <RouterProvider router={router} />
     </ThemeProvider>
   )
--- src/pages/_layout/index.tsx
+++ src/pages/_layout/index.tsx
@@ -1,13 +1,18 @@
 import { Provider as ReduxProvider } from 'react-redux'
 import { Outlet } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 
 import { Header } from '@/components/header'
+import { useTheme } from '@/hooks/useTheme'
 import { store } from '@/store'
 
 export function Layout() {
+  const { theme } = useTheme()
   return (
     <div className="mx-auto min-h-screen max-w-6xl p-6">
       <ReduxProvider store={store}>
+        <ToastContainer theme={theme} />
+
         <Header />
         <div className="pt-8">
           <Outlet />

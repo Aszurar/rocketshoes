889c662c51782e179bbf17cdd5ca99afc1b9485a
Aszurar <lms.souza39@gmail.com>
2025-03-13 06:40:51 -0300

    feat: adicionar descrição ao componente CustomAlert

--- src/components/custom-alert.tsx
+++ src/components/custom-alert.tsx
@@ -4,15 +4,21 @@ import { Alert, AlertTitle } from './ui/alert'
 
 type CustomAlertProps = {
   title: string
+  description?: string
   size?: 'xs' | 'sm' | 'md' | 'lg' | null
 }
 
-export function CustomAlert({ title, size }: Readonly<CustomAlertProps>) {
+export function CustomAlert({
+  title,
+  description,
+  size,
+}: Readonly<CustomAlertProps>) {
   return (
     <Alert
       variant="destructive"
       className="flex w-fit items-center justify-center gap-2"
       size={size}
+      title={description}
     >
       <AlertCircleIcon className="h-4 w-4" />
       <AlertTitle>{title}</AlertTitle>

602b7185b2693350743fcc67eb42582039c39da0
Aszurar <lms.souza39@gmail.com>
2025-03-13 06:37:05 -0300

    feat: adicionar suporte ao @tanstack/react-query no componente App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import './theme/global.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from 'react-router-dom'
 
 import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './router'
+import { queryClient } from './services/react-query'
 
 function App() {
   return (
     <ThemeProvider storageKey="rocketshoes-theme" defaultTheme="dark">
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ThemeProvider>
   )
 }

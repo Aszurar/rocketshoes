d38d8e1219c2c50b6848c4260989534457231f52
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2025-03-22 12:14:40 -0300

    feat: adicionar configuração do TypeScript, arquivos de sitemap e atualizar ícones

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
+import { defineConfig } from 'vite'
+import VitePluginSitemap from 'vite-plugin-sitemap'
 
+import { ROUTES } from './src/router/utils'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginSitemap({
+      hostname: 'https://rocketshoes-sand.vercel.app/',
+      // rotas estáticas
+      exclude: [ROUTES.checkout],
+    }),
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })

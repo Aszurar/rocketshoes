78f40f1628d10af42a3a2a017301cb60cf97d73b
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2025-03-24 19:14:07 -0300

    feat: reduzir o atraso nas requisições da API para 1 segundo

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,9 +6,11 @@ export const api = axios.create({
   baseURL: env.VITE_API_URL,
 })
 
+const ONE_SECOND = 1000
+
 if (env.VITE_ENABLE_API_DELAY) {
   api.interceptors.request.use(async (config) => {
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+    await new Promise((resolve) => setTimeout(resolve, ONE_SECOND))
     return config
   })
 }

a3f19b7d4557e16375c688d718b42b7eec95fd7f
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2025-03-29 08:01:31 -0300

    feat: refatorar componente ShoesCardImage para usar class-variance-authority e melhorar a responsividade

--- src/components/shoes-card-image.tsx
+++ src/components/shoes-card-image.tsx
@@ -1,38 +1,51 @@
-import { cn } from '@/lib/utils'
+import { cva, type VariantProps } from 'class-variance-authority'
 
+// Definição das variantes
+const imageContainer = cva('flex flex-shrink-0 overflow-hidden rounded-full', {
+  variants: {
+    size: {
+      xs: 'h-12 w-12',
+      sm: 'h-16 w-16',
+      md: 'h-20 w-20',
+    },
+  },
+  defaultVariants: {
+    size: 'sm',
+  },
+})
+
+// Precisamos do sizes para definir repassar
+// o tamanho da imagem para a tag img
 const sizes = {
   xs: 48,
   sm: 64,
   md: 80,
 } as const
 
-type SizesType = keyof typeof sizes
+type ImageContainerProps = VariantProps<typeof imageContainer>
 
-type ShoesCardImageProps = {
+type ShoesCardImageProps = ImageContainerProps & {
   image: string
   title: string
-  size?: SizesType
 }
 
 export function ShoesCardImage({
   image,
   title,
-  size = 'sm',
+  size,
 }: Readonly<ShoesCardImageProps>) {
-  const imageSize = sizes[size]
+  // Garantindo que size seja uma chave válida para sizes
+  const sizeKey = size ?? 'sm'
+  const imageSize = sizes[sizeKey]
+
   return (
-    <div
-      className={`min-h-[${imageSize}px] min-w-[${imageSize}px] flex flex-shrink-0 overflow-hidden rounded-full`}
-    >
+    <div className={imageContainer({ size })}>
       <img
         src={image}
         alt={title}
         width={imageSize}
         height={imageSize}
-        className={cn(
-          'rounded-full border-2 border-primary object-contain p-1',
-          `min-h-[${imageSize}px] min-w-[${imageSize}px] flex flex-shrink-0`,
-        )}
+        className="h-full w-full rounded-full border-2 border-primary object-cover p-1"
       />
     </div>
   )

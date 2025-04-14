7fe8f687c70743334f1634b8993698ac1b95507a
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2025-04-14 19:50:46 -0300

    fix: corrigir referência ao tempo de espera no interceptor da API

--- src/services/api.ts
+++ src/services/api.ts
@@ -9,7 +9,7 @@ export const api = axios.create({
 
 if (env.VITE_ENABLE_API_DELAY) {
   api.interceptors.request.use(async (config) => {
-    await new Promise((resolve) => setTimeout(resolve, TIME.ONE_SECOND))
+    await new Promise((resolve) => setTimeout(resolve, TIME.SECOND))
     return config
   })
 }

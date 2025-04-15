f2af46bbe944cd306629d5581138c1f3b2847af0
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2025-04-15 14:12:23 -0300

    fix: atualizar configuração do Cypress para usar a variável de ambiente CYPRESS_BASE_URL

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -3,12 +3,14 @@
 // cypress.config.cjs
 const { defineConfig } = require('cypress');
 
+const baseUrl = 'http://localhost:5173'
+
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
       return config;
     },
-    baseUrl: 'http://localhost:4173',
+    baseUrl: process.env.CYPRESS_BASE_URL || baseUrl,
   },
 });
